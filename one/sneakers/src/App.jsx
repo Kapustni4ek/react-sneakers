6f2c832a7a3cca799b4f7a82c8bb280e683ac7b3
kapustni4ek <superdenis517@gmail.com>
2022-11-17 14:13:21 +0300

    Five

--- one/sneakers/src/App.jsx
+++ one/sneakers/src/App.jsx
@@ -2,34 +2,21 @@ import React from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
-
-
-const arr = [
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12999,
-    imageUrl: '/img/image5.jpg',
-  },
-  {
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 12999,
-    imageUrl: '/img/image6.jpg',
-  },
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 8499,
-    imageUrl: '/img/image7.jpg',
-  },
-  {
-    title: 'Кроссовки Puma X Aka Boku Future Rider',
-    price: 8999,
-    imageUrl: '/img/image8.jpg',
-  },
-]
+ 
 
 function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
+  const [items, setItems] = React.useState([]);
 
+  React.useEffect(() => {
+    fetch('https://6375561208104a9c5f97b635.mockapi.io/Items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      }, [])
+  })
 
   return (
     <div className="wrapper clear">
@@ -46,8 +33,8 @@ function App() {
             <input type="text" placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
             <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
           ))}
         </div>
